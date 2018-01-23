1c4b176d40c5d9dff648204c734d6f12352c57f3
Maycol Uribe Romero <maycolur7@gmail.com>
2018-01-23 18:31:07 +0100

    Icon components for the player created

--- src/playlist/components/playlist.js
+++ src/playlist/components/playlist.js
@@ -1,11 +1,32 @@
 import React from 'react'
 import Media from './media'
+import Play from '../../icons/components/play'
+import Pause from '../../icons/components/pause'
+import Volume from '../../icons/components/Volume'
+import FullScreen from '../../icons/components/full-screen'
+
 
  function Playlist (props) {
   const playlist = props.data.categories[0].playlist
   console.log(props.data)
   return (
-    <div>
+    <div className="Playlist">
+      <Play
+        size={20}
+        color="red"
+      />
+      <Pause
+        size={20}
+        color="blue"
+      />
+      <Volume
+        size={20}
+        color="green"
+      />
+      <FullScreen
+        size={20}
+        color="blue"
+      />
       {
         playlist.map((item) => {
           return <Media {...item} key={item.id} />
